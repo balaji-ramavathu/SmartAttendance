f4bebf1e45d498f991b6f8cd24a5048a95c8e210
luvk1412 <luvk1412@gmail.com>
2019-03-09 05:01:37 +0530

    logins added

--- app/src/main/java/com/example/smartattendance/TeacherLoginActivity.java
+++ app/src/main/java/com/example/smartattendance/TeacherLoginActivity.java
@@ -1,17 +1,27 @@
 package com.example.smartattendance;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
+
 public class TeacherLoginActivity extends AppCompatActivity {
+    MaterialButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_login);
+        btnLogin=findViewById(R.id.btnTeacherLogin);
+
+
     }
 
     public void onClickBtnTeacherLogin(View view) {
+        Intent intent=new Intent(TeacherLoginActivity.this,TeacherDashboardActivity.class);
+        startActivity(intent);
     }
 }
